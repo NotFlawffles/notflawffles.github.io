ca5c53f1bc7254ede1b883a56f66fa2d9dcca978
NotFlawffles <hassenjasim3@gmail.com>
2025-06-05 00:05:56 +0300

    reinvention

---
+++ public/script/script.js
@@ -0,0 +1,36 @@
+const element = {
+    time: document.querySelector(".time"),
+    profilePicture: document.querySelector(".profile-picture"),
+    profilePictureShadow: document.querySelector(".profile-picture-shadow"),
+    github: document.querySelector("#github"),
+    discord: document.querySelector("#discord"),
+    reddit: document.querySelector("#reddit"),
+    youtube: document.querySelector("#youtube"),
+};
+
+element.github.addEventListener("click", () => {
+    window.open("https://github.com/notflawffles");
+});
+
+element.discord.addEventListener("click", () => {
+    window.open("https://discordapp.com/users/519826321552048129");
+});
+
+element.reddit.addEventListener("click", () => {
+    window.open("https://www.reddit.com/user/NotFlawffles/");
+});
+
+element.youtube.addEventListener("click", () => {
+    window.open("https://www.youtube.com/@notflawffles");
+});
+
+const updateTime = () => {
+    const time = new Date().toLocaleString("en-US", { timeZone: "Asia/Baghdad" }).replace(",", " 🞄  ");
+    element.time.textContent = time;
+};
+
+updateTime();
+
+setInterval(() => {
+    updateTime();
+}, 1000);
